ee9a02389e59720f6bcd10865232758be1db9bad
chrisjbillington <chrisjbillington@gmail.com>
2024-12-04 09:22:25 +1100

    Remove references to setuptools-conda from setup.p

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import sysconfig
 from pathlib import Path
 
-# Normally packages don't have to do this - the dist_conda command should be
+# Normally packages don't have to do this - the ci_distinfo command should be
 # automatically available. But since we're installing it, it isn't there yet!
 from ci_helper.ci_distinfo import ci_distinfo
 CMDCLASS = {'ci_distinfo': ci_distinfo}
@@ -16,7 +16,7 @@ VERSION_SCHEME = {
 
 SITE_PACKAGES = sysconfig.get_path('purelib')
 
-# Add the dist_conda command to the distlib vendored in setuptools:
+# Add the ci_distinfo command to the distlib vendored in setuptools:
 setuptools_distutils = Path(SITE_PACKAGES) / 'setuptools' / '_distutils' / 'command'
 DATA_FILES = [
     (str(setuptools_distutils.relative_to(sys.prefix)), ["ci_distinfo.py"]),
